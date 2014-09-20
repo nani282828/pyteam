d6b3148fe5ddc4160a0ad61f719ba78d085a18b5
nani282828 <nani282828@gmail.com>
2014-09-20 12:02:55 +0530

    successfully returned ajax posts and store

--- weber/home_func/models.py
+++ weber/home_func/models.py
@@ -1,13 +1,11 @@
 from mongoengine import *
 from weber.settings import DBNAME
 connect(DBNAME)
-import datetime
 
-
-# Create your models here.
-class UserPost(Document):
-    post_title = StringField(max_length=120,required=True)
+class Userpost(Document):
+    title = StringField(max_length=120,required=True)
     #image_path =
-    #publish_date1 = DateTimeField(default=datetime.datetime.now)
-    #permission_type = StringField()
-    user_name = StringField()
+    publish_date = DateTimeField()
+    username = StringField(max_length=120,required=True)
+    permission_type=StringField(max_length=120,required=True)
+
--- weber/home_func/views.py
+++ weber/home_func/views.py
@@ -1,25 +1,21 @@
-#from django.shortcuts import render
-from forms import PostStatusForm
-import json
 from django.shortcuts import render
 from django.shortcuts import *
-from django.template import RequestContext
 from django.http import HttpResponse
-from models import UserPost
-#from mongoengine.django.auth import User
+from models import Userpost
+#from django.contrib.auth import User
+from mongoengine.django.auth import User
+import json
 import datetime
 
 # Create your views here.
 def post_status(request):
     if request.method == 'POST':
         title = request.POST['post_text']
-        post = UserPost(post_title=title)
-        post.user_name = 'slkdflsjdfl'
-        #post.permission_type = request.POST['permission_type']
-        #post.user_name = request.user.username
-        #publish_date1 = datetime.datetime.now()
-        #post.publish_date1 = publish_date1
-        post.save(force_insert=True,validate=False, clean=False)
-        return HttpResponse(json.dumps({'post_title': request.POST['post_text']}))
+        post = Userpost(title=title)
+        post.username = request.user.username
+        post.publish_date = datetime.datetime.now()
+        post.permission_type = request.POST['permission_type']
+        post.save()
+        return HttpResponse(json.dumps({'post_title': request.POST['post_text'],'publish_date':str(datetime.datetime.now()),'permission_type':request.POST['permission_type']}))
     else:
         return render(request,'homepage.html',{'userpost':'no post posted'})
